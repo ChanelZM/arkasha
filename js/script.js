da9b1ae512aee1b6d8dbe031ed0f14be029f6efe
Chanel Mepschen <chanelmepschen856@hotmail.com>
2017-12-11 18:39:27 +0100

    Trying to fix bug

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 (function(){
     var spans = document.querySelectorAll('.insert-text'),
-        wordList = document.querySelector('.word-list'),
-        wordElem = document.querySelectorAll('.word-list__item'),
         wordContainer = document.querySelectorAll('.opening-word__word-list');
 
     var words = ['handelsbelemmering', 'aanstichting', 'smeernippel', 'vrachtlijst', 'spraakcentrum', 'doek', 'zuiderzon'];
@@ -13,26 +11,37 @@
         open: function(e){
             var offsetX = e.target.offsetLeft,
                 offsetY = e.target.offsetTop,
-                i;
+                optionList = e.target.parentNode.children[1],
+                options = e.target.parentNode.children[1].children;
 
-            for(i = 0; i < wordElem.length; i++){
-                wordElem[i].innerHTML = randomWord();
-            }
+            addRandomWords(e, options);
 
-            wordList.removeAttribute('hidden');
-            wordList.style.left = offsetX + 'px';
-            wordList.style.top = offsetY + 112 + 'px';
+            optionList.removeAttribute('hidden');
+            optionList.style.left = offsetX + 'px';
+            optionList.style.top = offsetY + 112 + 'px';
         },
-        close: function(){
-            wordList.setAttribute('hidden', 'true');
+        close: function(e){
+            e.target.children[1] ? e.target.children[1].setAttribute('hidden', 'true') : e.target.parentNode.children[1].setAttribute('hidden', 'true');
         }
     };
 
-    function randomWord(){
+    function addRandomWords(e, options){
+        var randomWords = [];
+        var i;
+
+        for(i = 0; i < options.length; i++){
+            options[i].innerHTML = createRandomWord(e, randomWords);
+        }
+    }
+
+    function createRandomWord(e, array){
         var randomNum = Math.floor(Math.random() * words.length);
 
         //Preventing that two or three of the same word will be loaded into the html
         if(last == words[randomNum]){
+            randomNum = Math.floor(Math.random() * words.length);
+            last = words[randomNum];
+        } else if(e.target.innerHTML == words[randomNum]){
             randomNum = Math.floor(Math.random() * words.length);
             last = words[randomNum];
         } else {
@@ -45,7 +54,11 @@
         var i;
         for(i = 0; i < spans.length; i++){
             words.push(spans[i].innerHTML);
-            spans[i].addEventListener('mouseover', list.open);
+            spans[i].addEventListener('mouseover', function(e){
+                if(e.target.parentNode.children[1].hasAttribute('hidden')){
+                    list.open(e);
+                }
+            });
         }
         for(i = 0; i < wordContainer.length; i++){
             wordContainer[i].addEventListener('mouseleave', list.close);
