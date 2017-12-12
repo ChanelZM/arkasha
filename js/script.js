f2abac8edc5b659860beb75347a54a90f2585ca8
Chanel Mepschen <chanelmepschen856@hotmail.com>
2017-12-12 12:44:30 +0100

    Typing text is now working

--- js/script.js
+++ js/script.js
@@ -3,41 +3,75 @@
     var spans = document.querySelectorAll('.insert-text');
 
     var words = ['handelsbelemmering', 'aanstichting', 'smeernippel', 'vrachtlijst', 'spraakcentrum', 'doek', 'zuiderzon'],
-        count = -1;
+        count = -1,
+        num = 0,
+        isAnimating = false,
+        speed = 200;
 
     var nextWord;
 
     function init(){
         var i;
         for(i = 0; i < spans.length; i++){
-            spans[i].addEventListener('mouseover', hasElemHaveText);
+            spans[i].addEventListener('mouseover', checkIfStillAnimating);
+            spans[i].innerHTML = 'hover here';
+        }
+    }
+
+    function checkIfStillAnimating(e){
+        if(isAnimating == false){
+            hasElemHaveText(e);
         }
     }
 
     function hasElemHaveText(e){
-        e.target.innerHTML == '' ? getNextWord(e) : eraseWord(e);
+        isAnimating = true;
+        e.target.innerHTML == 'hover here' ? getNextWord(e) : eraseWord(e, e.target.innerHTML);
     }
 
     function getNextWord(e){
+        var elem = e.target || e;
+
         count == (words.length - 1) ? count = 0: count++;
         nextWord = words[count];
-        e.target.innerHTML = '';
 
-        typeWord(nextWord);
+        elem.innerHTML = '';
+
+        typeWord(elem, nextWord);
     }
 
-    function typeWord(word){
-        //Type word in span
+    function typeWord(elem, word){
+        num = 0;
+
+        loopThroughLetters(elem, 'add', word, word.length);
+
+        setTimeout(function(){
+            isAnimating = false;
+        }, (speed * word.length));
     }
 
-    function eraseWord(e){
-        //Erase word in span
-        //Then type word again
+    function eraseWord(e, prevWord){
+        num = prevWord.length;
+
+        loopThroughLetters(e.target, 'subtract', prevWord, 0);
     }
 
-    function animateWord(elem, type){
-        //Animate word depending on erasing or typing
+    function loopThroughLetters(elem, type, word, max){
+        type == 'add' ? num++ : num--;
+
+        elem.innerHTML = word.substring (0, num);
+
+        setTimeout(function(){
+            if(type == 'add' && num < max){
+                loopThroughLetters(elem, type, word, max);
+            } else if(type == 'subtract' && num > max){
+                loopThroughLetters(elem, type, word, max);
+            } else if(type == 'subtract' && num == max){
+                getNextWord(elem);
+            }
+        }, speed);
     }
 
     init();
+
 })();
